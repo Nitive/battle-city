11cab1333105b5c401b41512818efacbe997faef
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-10-29 08:54:46 +0300

    chore: add webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
+    filename: '[name].[hash].js',
   },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      query: {
+        cacheDirectory: true,
+      },
+    }, {
+      test: /\.(png|jpg)/,
+      loader: 'file',
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
