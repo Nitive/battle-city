dbb133871388e0b73dcf380770c4fe4f89d66fc9
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-10-29 09:10:24 +0300

    chore: add cycle.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // @flow
 
+import xs from 'xstream'
+import { run } from '@cycle/xstream-run'
+import { div, makeDOMDriver } from '@cycle/dom'
+
 export const sum = (...args: number[]) => {
   return args.reduce((acc, x) => acc + x, 0)
 }
+
+function main() {
+  return {
+    DOM: xs.of(div('Hello, world!')),
+  }
+}
+
+run(main, { DOM: makeDOMDriver('#app') })
