2832f98720f38124038fbd461c48da32c9d88c09
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-10-30 20:45:34 +0300

    chore(lint): add lint to pretest task

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const { env } = process
 const assetsPort = env.ASSETS_PORT || '5000'
 
 module.exports = {
-  isDev:  ['development', 'dev', ''].includes(env.NODE_ENV),
+  isDev: ['development', 'dev', ''].includes(env.NODE_ENV),
   isProd: env.NODE_ENV === 'production',
   app: {
     port: env.APP_PORT || '3000',
