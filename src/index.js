218d9151756639fe06d9f876f762788b28c3620c
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-11-05 12:25:26 +0300

    change counter to position counter

--- src/index.js
+++ src/index.js
@@ -5,16 +5,28 @@ import { run } from '@cycle/xstream-run'
 import { div, makeDOMDriver } from '@cycle/dom'
 import { makeKeyboardDriver } from 'cycle-keyboard'
 
+type Position = {
+  x: number;
+  y: number;
+}
+
 function main({ keyboard }) {
   const action$ = xs.merge(
-    keyboard.downs('up').mapTo(+1),
-    keyboard.downs('down').mapTo(-1)
+    keyboard.downs('up').mapTo({ x: +1, y: 0 }),
+    keyboard.downs('down').mapTo({ x: -1, y: 0 }),
+    keyboard.downs('left').mapTo({ x: 0, y: -1 }),
+    keyboard.downs('right').mapTo({ x: 0, y: +1 }),
   )
 
-  const position$ = action$.fold((sum, x) => sum + x, 0)
+  const start: Position = { x: 0, y: 0 }
+
+  const position$ = action$.fold((position: Position, diff) => ({
+    x: position.x + diff.x,
+    y: position.y + diff.y,
+  }), start)
 
   const vdom$ = position$.map(position => {
-    return div(`Position is ${position}`)
+    return div(`Position is ${position.x}, ${position.y}`)
   })
 
   return {
