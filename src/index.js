d8ed7bd691ab200da016f9b8088a096f5c0f62a3
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-11-05 12:42:08 +0300

    move updatePosition into separate function

--- src/index.js
+++ src/index.js
@@ -4,28 +4,18 @@ import xs from 'xstream'
 import { run } from '@cycle/xstream-run'
 import { div, makeDOMDriver } from '@cycle/dom'
 import { makeKeyboardDriver } from 'cycle-keyboard'
-import { clamp } from 'ramda'
-
-type Position = {
-  x: number;
-  y: number;
-}
-
-const updatePosition = (position: Position, diff: Position) => ({
-  x: clamp(0, 800, position.x + diff.x),
-  y: clamp(0, 600, position.y + diff.y),
-})
+import { updatePosition } from './utils/position'
 
 function main({ keyboard }) {
   const speed = 5
   const action$ = xs.merge(
-    keyboard.downs('up').mapTo({ x: speed, y: 0 }),
+    keyboard.downs('up').mapTo({ x: +speed, y: 0 }),
     keyboard.downs('down').mapTo({ x: -speed, y: 0 }),
     keyboard.downs('left').mapTo({ x: 0, y: -speed }),
-    keyboard.downs('right').mapTo({ x: 0, y: speed }),
+    keyboard.downs('right').mapTo({ x: 0, y: +speed }),
   )
 
-  const start: Position = { x: 0, y: 0 }
+  const start = { x: 0, y: 0 }
 
   const position$ = action$.fold(updatePosition, start)
 
--- src/utils/sum.js
+++
@@ -1,4 +0,0 @@
-export const sum = (arr: number[]) => {
-  return arr.reduce((acc, x) => acc + x, 0)
-}
-
