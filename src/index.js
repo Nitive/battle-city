9a8da95a2da323ed4903996ce01b60e6a7683413
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-11-05 12:06:46 +0300

    feat: add counter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@
 import xs from 'xstream'
 import { run } from '@cycle/xstream-run'
 import { div, makeDOMDriver } from '@cycle/dom'
+import { makeKeyboardDriver } from 'cycle-keyboard'
 
-function main() {
+function main({ keyboard }) {
+  const action$ = xs.merge(
+    keyboard.downs('up').mapTo(+1),
+    keyboard.downs('down').mapTo(-1)
+  )
+
+  const position$ = action$.fold((sum, x) => sum + x, 0)
+
+  const vdom$ = position$.map(position => {
+    return div(`Position is ${position}`)
+  })
+
   return {
-    DOM: xs.of(div('Hello, world!')),
+    DOM: vdom$,
   }
 }
 
-run(main, { DOM: makeDOMDriver('#app') })
+run(main, {
+  DOM: makeDOMDriver('#app'),
+  keyboard: makeKeyboardDriver(),
+})
