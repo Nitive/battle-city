ddc261209f61b9a994858a9c2c7d9f5f930ae8c0
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-11-07 00:18:40 +0700

    Add cycle.js related libs (#8)
    * feat: add dependencies
    
    * update yarn.lock
    
    * remove cycle-canvas
    
    * install cycle-keyboard
    
    * remove cycle-animation-driver
    
    * feat: add counter
    
    * change counter to position counter
    
    * add ramda
    
    * add position borders
    
    * add speed
    
    * move updatePosition into separate function
    
    * update dependencies
    
    * add interfaces for ramda

---
+++ interfaces/ramda.js
@@ -0,0 +1,5 @@
+declare module 'ramda' {
+  declare module.exports: {
+    clamp(rangeStart: string, rangeEnd: number, num: number): number;
+  }
+}
---
+++ src/utils/position.js
@@ -0,0 +1,11 @@
+import { clamp } from 'ramda'
+
+type Position = {
+  x: number;
+  y: number;
+}
+
+export const updatePosition = (position: Position, diff: Position) => ({
+  x: clamp(0, 800, position.x + diff.x),
+  y: clamp(0, 600, position.y + diff.y),
+})
