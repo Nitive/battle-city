470841f7ba8866c57650bdc9885f8439b676ca57
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-12-10 07:55:31 +0300

    Remove time from state

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -9,7 +9,6 @@ export default function reducer(state: State, action: Action): State {
     case 'ChangeDirection':
       return {
         position: state.position,
-        time: state.time,
         direction: action.payload.direction,
         lastDirection: action.payload.direction || state.lastDirection,
       }
@@ -19,7 +18,6 @@ export default function reducer(state: State, action: Action): State {
         position: state.direction
           ? step(state.position, state.direction, speed)
           : state.position,
-        time: state.time + 50,
         direction: state.direction,
         lastDirection: state.lastDirection,
       }
