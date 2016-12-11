b2045fce319ffa8c7840a89d2a2944184d6c6e17
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-12-11 19:57:49 +0300

    test: use describe/it instead of test

--- src/view/components/tank/test.ts
+++ src/view/components/tank/test.ts
@@ -4,20 +4,23 @@ import { toHTML } from '../../__tests__/test-helpers'
 
 import tank from '.'
 
-function check(position: Position, direction: Direction, expectedContent: string) {
-  const positionString = `{ x: ${position.x}, y: ${position.y} }`
-  test(`tank with position ${positionString} should have '${expectedContent}'`, async () => {
-    const html = await toHTML(tank(position, direction))
-    expect(html).toContain(expectedContent)
+describe('tank', () => {
+  function check(position: Position, direction: Direction, expectedContent: string) {
+    const positionString = `{ x: ${position.x}, y: ${position.y} }`
+    it(`with position ${positionString} should have '${expectedContent}'`, async () => {
+      const html = await toHTML(tank(position, direction))
+      expect(html).toContain(expectedContent)
+    })
+  }
+
+  check({ x: 0, y: 0 }, Direction.Up, '"rotate(0 30 30)translate(0 0)"')
+  check({ x: 10, y: 0 }, Direction.Down, '"rotate(180 40 30)translate(10 0)"')
+  check({ x: 0, y: 100 }, Direction.Right, '"rotate(90 30 130)translate(0 100)"')
+  check({ x: 200, y: 100 }, Direction.Left, '"rotate(-90 230 130)translate(200 100)"')
+
+  it(`default direction should be right`, async () => {
+    const html = await toHTML(tank({ x: 0, y: 0 }))
+    expect(html).toContain('"rotate(90 30 30)translate(0 0)"')
   })
-}
 
-check({ x: 0, y: 0 }, Direction.Up, '"rotate(0 30 30)translate(0 0)"')
-check({ x: 10, y: 0 }, Direction.Down, '"rotate(180 40 30)translate(10 0)"')
-check({ x: 0, y: 100 }, Direction.Right, '"rotate(90 30 130)translate(0 100)"')
-check({ x: 200, y: 100 }, Direction.Left, '"rotate(-90 230 130)translate(200 100)"')
-
-test(`default direction is right`, async () => {
-  const html = await toHTML(tank({ x: 0, y: 0 }))
-  expect(html).toContain('"rotate(90 30 30)translate(0 0)"')
 })
