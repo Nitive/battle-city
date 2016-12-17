1041ea85fed0856958a4988dd59c00d97d4d80b3
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-12-17 10:40:06 +0300

    test(position): add test for new updatePosition function

--- src/state/utils/__tests__/position.test.ts
+++ src/state/utils/__tests__/position.test.ts
@@ -6,16 +6,26 @@ function checkUpdatePosition(start: Position, diff: Position, result: Position) 
 }
 
 describe('updatePosition', () => {
-  it('updatePosition should work', () => {
+  it('should work', () => {
     checkUpdatePosition({ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 0 })
     checkUpdatePosition({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 1 })
     checkUpdatePosition({ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 1 })
     checkUpdatePosition({ x: 30, y: 50 }, { x: 0, y: 1 }, { x: 30, y: 51 })
   })
 
-  it('updatePosition should has limits', () => {
+  it('should has limits', () => {
     checkUpdatePosition({ x: 0, y: 0 }, { x: -1, y: -1 }, { x: 0, y: 0 })
-    checkUpdatePosition({ x: 0, y: 0 }, { x: 1000, y: 1000 }, { x: 740, y: 540 })
+    checkUpdatePosition({ x: 0, y: 0 }, { x: 1000, y: 1000 }, { x: 800, y: 600 })
+  })
+
+  it('should set custom right&down limits', () => {
+    const position = updatePosition(80, 100)({ x: 0, y: 0 }, { x: 1000, y: 1000 })
+    expect(position).toEqual({ x: 720, y: 500 })
+  })
+
+  it('should set custom left&up limits', () => {
+    const position = updatePosition(100, 100, 50, 30)({ x: 0, y: 0 }, { x: 0, y: 0 })
+    expect(position).toEqual({ x: 50, y: 30 })
   })
 })
 
