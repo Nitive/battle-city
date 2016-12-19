1774c2ae5d1a90889590bdd146c7ef27b969196d
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-12-19 09:20:26 +0300

    test(reducer): fix tests for new state structure

--- src/state/__tests__/reducer.test.ts
+++ src/state/__tests__/reducer.test.ts
@@ -16,104 +16,131 @@ const defaultState: State = {
 
 describe('Tick:', () => {
   it('should move up', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 100 },
-      direction: Direction.Up,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 50, y: 100 },
+        direction: Direction.Up,
+      },
     }
 
     const { tank } = reducer(state, actions.tick())
-    expect(tank.position.y).toBeLessThan(state.position.y)
-    expect(tank.position.x).toBe(state.position.x)
+    expect(tank.position.y).toBeLessThan(state.tank.position.y)
+    expect(tank.position.x).toBe(state.tank.position.x)
   })
 
   it('should move down', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 100 },
-      direction: Direction.Down,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 50, y: 100 },
+        direction: Direction.Down,
+      },
     }
 
     const { tank } = reducer(state, actions.tick())
-    expect(tank.position.y).toBeGreaterThan(state.position.y)
-    expect(tank.position.x).toBe(state.position.x)
+    expect(tank.position.y).toBeGreaterThan(state.tank.position.y)
+    expect(tank.position.x).toBe(state.tank.position.x)
   })
 
   it('should move right', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 0 },
-      direction: Direction.Right,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 50, y: 50 },
+        direction: Direction.Right,
+      },
     }
 
     const { tank } = reducer(state, actions.tick())
-    expect(tank.position.x).toBeGreaterThan(state.position.x)
-    expect(tank.position.y).toBe(state.position.y)
+    expect(tank.position.x).toBeGreaterThan(state.tank.position.x)
+    expect(tank.position.y).toBe(state.tank.position.y)
   })
 
   it('should move left', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 100, y: 0 },
-      direction: Direction.Left,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 100, y: 50 },
+        direction: Direction.Left,
+      },
     }
 
     const { tank } = reducer(state, actions.tick())
-    expect(tank.position.x).toBeLessThan(state.position.x)
-    expect(tank.position.y).toBe(state.position.y)
+    expect(tank.position.x).toBeLessThan(state.tank.position.x)
+    expect(tank.position.y).toBe(state.tank.position.y)
   })
 
   it('should has up limit', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 0 },
-      direction: Direction.Up,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 0, y: 0 },
+        direction: Direction.Up,
+      },
     }
 
     expect(reducer(state, actions.tick())).toEqual(state)
   })
 
   it('should has down limit', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 2000 },
-      direction: Direction.Down,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 0, y: 2000 },
+        direction: Direction.Down,
+      },
     }
 
   const { tank } = reducer(state, actions.tick())
-    expect(tank.position.y).toBeLessThanOrEqual(state.position.y)
-    expect(tank.position.x).toBe(state.position.x)
+    expect(tank.position.y).toBeLessThanOrEqual(state.tank.position.y)
+    expect(tank.position.x).toBe(state.tank.position.x)
   })
 
   it('should has left limit', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 0 },
-      direction: Direction.Left,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 0, y: 0 },
+        direction: Direction.Left,
+      },
     }
 
     expect(reducer(state, actions.tick())).toEqual(state)
   })
 
   it('should has right limit', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 2000, y: 0 },
-      direction: Direction.Right,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 2000, y: 0 },
+        direction: Direction.Right,
+      },
     }
 
     const { tank } = reducer(state, actions.tick())
-    expect(tank.position.x).toBeLessThanOrEqual(state.position.x)
-    expect(tank.position.y).toBe(state.position.y)
+    expect(tank.position.x).toBeLessThanOrEqual(state.tank.position.x)
+    expect(tank.position.y).toBe(state.tank.position.y)
   })
 })
 
 describe('ChangeDirection:', () => {
   it('should change direction to right', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 0 },
-      direction: Direction.Up,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 0, y: 0 },
+        direction: Direction.Up,
+      },
     }
 
     const { tank } = reducer(state, actions.changeDirection(Direction.Right))
@@ -122,10 +149,13 @@ describe('ChangeDirection:', () => {
   })
 
   it('should change direction to up', () => {
-    const state = {
+    const state: State = {
       ...defaultState,
-      position: { x: 0, y: 0 },
-      direction: Direction.Down,
+      tank: {
+        ...defaultState.tank,
+        position: { x: 0, y: 0 },
+        direction: Direction.Down,
+      },
     }
 
     const { tank } = reducer(state, actions.changeDirection(Direction.Up))
